2d6994d1d63c425107c43387c080d0671b06ca6a
Void <coding-tonight>
2024-10-01 07:02:26 +0545

    floating navbar added

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,13 +1,52 @@
+import { useState } from "react";
+
+import {
+  motion,
+  AnimatePresence,
+  useScroll,
+  useMotionValueEvent,
+} from "framer-motion";
+
 import {
     Tabs,
-    TabsContent,
     TabsList,
     TabsTrigger,
   } from '@/components/ui/tabs' 
 
 const Menu = () => {
+  const [visible , setVisible] = useState(false)
+  const  { scrollYProgress  } = useScroll()
+
+  useMotionValueEvent(scrollYProgress, "change", (current) => {
+    // Check if current is not undefined and is a number
+    if (typeof current === "number") {
+      // let direction = current! - scrollYProgress.getPrevious()!;
+
+      if(current > 0.05) {
+         setVisible(true)
+      } else {
+        setVisible(false)
+      }
+    }
+  })
+
     return (
-      <section className='container w-[80vw] mx-auto sticky top-1 flex justify-center'>
+      <AnimatePresence mode="wait">
+      <motion.div
+        initial={{
+          opacity: 1,
+          y: -100,
+        }}
+        animate={{
+          y: visible ? 0 : -100,
+          opacity: visible ? 1 : 0,
+        }}
+        transition={{
+          duration: 0.2,
+        }}
+        className="fixed top-1"
+      >
+      <section className='container w-[100vw] mx-auto flex justify-center'>
         <Tabs defaultValue='projects'>
             <TabsList className='shadow-md border border-black rounded-2xl p-5'>
               <TabsTrigger value='projects'>Projects</TabsTrigger>
@@ -18,6 +57,8 @@ const Menu = () => {
             </TabsList>
         </Tabs>
       </section>
+      </motion.div>
+      </AnimatePresence>
     )
 }
 
--- src/layouts/GuestLayout.tsx
+++ src/layouts/GuestLayout.tsx
@@ -4,18 +4,20 @@ import { Timeline } from "@/components/ui/timeline"
 const GuestLayout:React.FC = () => {
     return (
         <>  
-        <Navbar  />
-        <main style={{
-           height: 'calc(100dvh - 30px)'
-        }}>
-          <HeroSection />
-          <Menu />
-          {/* <Tools /> */}
-          <Timeline data={[
-             { title: 'Student', content: 'Business Studies'},
-             { title: 'Frontend Developer', content: 'APA (Asia Population Association)'},
-             { title: 'Full Stack Developer', content: 'APA (Asia Population Association)'}]} />
-        </main>
+        <div className="relative  w-full">      
+            <Navbar  />
+            <Menu />
+            <main style={{
+                height: 'calc(100dvh - 30px)'
+            }}>
+            <HeroSection />
+            {/* <Tools /> */}
+            <Timeline data={[
+                { title: 'Student', content: 'Business Studies'},
+                { title: 'Frontend Developer', content: 'APA (Asia Population Association)'},
+                { title: 'Full Stack Developer', content: 'APA (Asia Population Association)'}]} />
+            </main>
+        </div>
         </>
     )
 }
